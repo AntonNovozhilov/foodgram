e485a6bf4a3ceef15e174b8792438fb367efbff8
AntonNovozhilov <wowlass63@gmail.com>
2025-03-18 23:00:57 -0500

    Начал писать модели

---
+++ backend/recipes/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Ingridients(models.Model):
+
+    title = models.CharField(
+        max_length=256, verbose_name="Название ингридиентов", unique=True
+    )
+    unit = models.CharField(
+        max_length=15, verbose_name="Единица измерения", unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+
+
+
+class Tag(models.Model):
+
+    title = models.CharField(max_length=256, verbose_name="Навзвание тэга")
+    slug = models.SlugField(max_length=256,
+                            verbose_name="Слаг",
+                            auto_created=True
+                        )
+    
+    class Meta:
+    verbose_name = 'Тэг'
+    verbose_name_plural = 'Тэги'
+
+
+class Recipes(models.Model):
+
+    Author = models.ForeignKey(User,
+                               verbose_name="Автор",
+                               on_delete=models.CASCADE
+                            )
+    title = models.CharField(
+        max_length=256, verbose_name="Название рецепта", unique=True
+    )
+    image = models.ImageField(
+        upload_to="images", verbose_name="Фотография блюда", unique=True
+    )
+    text = models.TextField(max_length=650, verbose_name="Описание рецепта")
+    ingridients = models.ManyToManyField(Ingridients,
+                                    verbose_name="Ингридиенты для рецепта",
+                                    unique=True,
+                                )
+    tags = models.ManyToManyField(Tag,
+                             verbose_name="Теги рецепта",
+                             unique=True
+                            )
+    time = models.PositiveSmallIntegerField(
+        verbose_name="Время приготовления в минутах", unique=True
+    )
+
+    class Meta:
+    verbose_name = 'Рецепт'
+    verbose_name_plural = 'Рецепты'
---
+++ backend/users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class MyUser(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name='Никнейм пользователя'
+    )
+    first_name = models.CharField(max_length=150, unique=True, verbose_name='Имя пользователя')
+    last_name = models.CharField(max_length=150, unique=True, verbose_name='Фамилия пользователя')
+    email = models.EmailField(max_length=256, unique=True, verbose_name='Почта пользователя')
+    password = models.CharField(max_length=100, unique=True, verbose_name='Пароль пользователя')
+
+    
